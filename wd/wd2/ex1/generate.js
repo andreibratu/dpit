cc2922d4a73fd9ad6128a08fc0be9070dbe4c855
Andrei Bratu <bratu.andrei@outlook.co>
2019-07-29 15:59:03 +0300

    Pentru raul

--- wd/wd2/ex1/generate.js
+++ wd/wd2/ex1/generate.js
@@ -1,13 +1,20 @@
 function generate()
 {
-  for(i = 0; i < 10; i++)
+  container = document.getElementById('container');
+  for(i = 0; i < 4; i++)
   {
+    // texty_boi = document.createElement('p')
+    // texty_boi.innerText = i;
+    // texty_boi.style.color = 'white'
+
     node = document.createElement('div');
     colors = ['red', 'yellow', 'blue', 'green'];
     color = colors[Math.floor(Math.random() * colors.length)];
+    node.innerHTML = i;
     node.className = color + ' ' + 'form';
-    // TODO var textnode = document.createTextNode("Water");
-    container = document.getElementById('container');
+    node.style.cssText = "text-align: center; color: white;"
+    // node.appendChild(texty_boi)
+
     container.appendChild(node);
   }
 }
